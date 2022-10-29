4fe394babd0f81d4165b5a5705c96eea0f3ba9c7
Morteza <morteza.torkashvand70@gmail.com>
2022-10-29 13:12:03 +0330

    work on react-router-dom

--- src/app.js
+++ src/app.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
+import Navbar from './components/navbar';
+import Home from './components/home';
 import Users from './components/users';
+import Login from './components/login';
+import Register from './components/register';
+import {Route, Routes} from 'react-router-dom';
+
 
 class App extends Component {
   render() {
-		return <Users/>
-  }
+		return (
+			<>
+				<Navbar/>
+				<div className='container mt-3'>
+					<Routes>
+						<Route path='/users' element={<Users/>} />
+						<Route path='/login' element={<Login/>} />
+						<Route path='/register' element={<Register/>} />
+						<Route path='/' element={<Home/>} />
+					</Routes>
+				</div>
+			</>
+		)
+   }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter } from 'react-router-dom';
 import App from './app';
 
-// ReactDOM.render(<App/>, document.getElementById('root'));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App/>);
+root.render(<BrowserRouter><App/></BrowserRouter>);
