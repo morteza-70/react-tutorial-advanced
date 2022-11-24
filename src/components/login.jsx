1544b2b043115a8d91c2af53452335f4ecc8005a
Morteza <morteza.torkashvand70@gmail.com>
2022-11-24 13:02:41 +0330

    vaildate in form in react js via yup library validate

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,22 +1,42 @@
 import React, { Component } from 'react';
 import Input from './input';
+import * as yup from 'yup';
+import axios from 'axios';
 class Login extends Component {
-
-	// login with ref module
-	// email = createRef();
-	// password = createRef();
-
-	// handleSubmit = async (e) => {
-	// 	e.preventDefault();
-	// 	const account = {email: this.email.current.value, password: this.password.current.value}; //current is Element and value is value input
-	// 	const response = await axios.post(`https://reqres.in/api/login`, account);
-	// 	console.log(response);
-	// };
 
 	state = {
 		account: {
 			email:'',
 			password: '',
+		},
+		errors:[],
+	};
+
+	schema = yup.object().shape({
+		email: yup.string()
+			.email('فرمت ایمیل صحیح نمی باشد')
+			.required('ایمیل الزامی می باشد'),
+		password: yup.string()
+			.required('پسورد الزامی می باشد')
+			.min(4, 'پسورد باید حداقل 4 کاراکتر باشد'),
+	});
+
+	validate = async () => {
+		try {
+			const result = await this.schema.validate(this.state.account, {abortEarly: false});
+			return result;			
+		} catch (error) {
+			this.setState({errors: error.errors})
+		}
+	};
+
+	handleSubmit = async (e) => {
+		e.preventDefault();
+		const result = await this.validate();
+		console.log(result);
+		if (result) {
+			const response = await axios.post('https://reqres.in/api/login', result);
+			console.log(response);
 		}
 	};
 
@@ -29,17 +49,27 @@ class Login extends Component {
 	render() {
 		const {email, password} = this.state.account;
 		return (
-			<form onSubmit={this.handleSubmit} style={{width: '23rem'}}>
-				<h3 className="fw-normal mb-3 pb-3" style={{letterSpacing: '1px'}}>Log in</h3>				
-				<Input onChange={this.handleChange} value={email} name="email" label="Email Address:"/>
-				<Input onChange={this.handleChange} value={password} name="password" label="Password:"/>
-				<div className="pt-1 mb-4">
-					<button className="btn btn-info btn-lg btn-block">Login</button>
-				</div>
-
-				{/* <p className="small mb-5 pb-lg-2"><a className="text-muted" href="#!">Forgot password?</a></p>
-				<p>Don't have an account? <a href="#!" className="link-info">Register here</a></p> */}
-			</form>
+			<>
+				{
+					this.state.errors.length !== 0 && (
+						<div className='alert alert-danger'>
+							<ul>
+								{
+									this.state.errors.map((e,i) => <li key={i}>{e}</li>)
+								}
+							</ul>
+						</div>
+					)
+				}
+				<form onSubmit={this.handleSubmit} style={{width: '23rem'}}>
+					<h3 className="fw-normal mb-3 pb-3" style={{letterSpacing: '1px'}}>Log in</h3>				
+					<Input onChange={this.handleChange} value={email} name="email" label="Email Address:"/>
+					<Input onChange={this.handleChange} value={password} name="password" label="Password:"/>
+					<div className="pt-1 mb-4">
+						<button className="btn btn-info btn-lg btn-block">Login</button>
+					</div>
+				</form>
+			</>
 		)
 	}
 };
